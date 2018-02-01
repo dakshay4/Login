580b279133f59c381af12bfc15c25663aad76763
dakshayagarwal <dakshsince96@gmail.com>
2018-02-01 16:28:37 +0530

    Database for the version1.0

---
+++ login.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: login
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.17.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `PROFILE`
+--
+
+DROP TABLE IF EXISTS `PROFILE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `PROFILE` (
+  `USERID` varchar(255) NOT NULL,
+  `FIRST_NAME` varchar(255) DEFAULT NULL,
+  `LAST_NAME` varchar(255) DEFAULT NULL,
+  `ADDRESS` varchar(255) DEFAULT NULL,
+  `CITY` varchar(255) DEFAULT NULL,
+  `PIN` int(11) DEFAULT NULL,
+  `DOB` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`USERID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `PROFILE`
+--
+
+LOCK TABLES `PROFILE` WRITE;
+/*!40000 ALTER TABLE `PROFILE` DISABLE KEYS */;
+INSERT INTO `PROFILE` VALUES ('dakshay','DAKSHAY','AGARWAL','S-4/3, Gopal Towers, Maharaja Agrasen Marg, Hari Marg, Malviya Nagar','Rajasthan',302017,'1996-12-04');
+/*!40000 ALTER TABLE `PROFILE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `REGISTER`
+--
+
+DROP TABLE IF EXISTS `REGISTER`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `REGISTER` (
+  `ID` int(11) NOT NULL,
+  `USERID` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) DEFAULT NULL,
+  `PASS` varchar(255) DEFAULT NULL,
+  `MOBILE_NO` bigint(20) DEFAULT NULL,
+  `PROFILE` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `REGISTER`
+--
+
+LOCK TABLES `REGISTER` WRITE;
+/*!40000 ALTER TABLE `REGISTER` DISABLE KEYS */;
+INSERT INTO `REGISTER` VALUES (1,'dakshay','dakshay@outlook.in','11',9799477161,1);
+/*!40000 ALTER TABLE `REGISTER` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-01 16:26:09
